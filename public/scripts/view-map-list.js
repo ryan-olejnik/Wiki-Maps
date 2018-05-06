23e049fe053e2ac0f4b9ad9d2e6d19f2cd1f2566
Justin <j.lum07@gmail.com>
2018-05-06 02:00:06 -0400

    🚀 added list to view map page

--- public/scripts/view-map-list.js
+++ public/scripts/view-map-list.js
@@ -1,4 +1,18 @@
 $( document ).ready(function() {
 
+  $(".poi-container").on("click", ".poi-title", function() {
+
+    console.log('bang');
+
+    // $(this).find(".poi-body").slideToggle("slow");
+
+    // $(this).next().find(".poi-body").slideToggle("slow");
+
+
+    $(this).next().slideToggle("slow");
+
+
+  });
+
 });
 

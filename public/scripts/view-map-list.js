4c7783e91d89f3f22ecfdcabe80059659f612ccf
Justin <j.lum07@gmail.com>
2018-05-06 15:54:17 -0400

    👩‍⚕️ fixed my revert!!

--- public/scripts/view-map-list.js
+++ public/scripts/view-map-list.js
@@ -1,16 +1,26 @@
 $( document ).ready(function() {
 
-  $(".poi-container").on("click", ".poi-title", function() {
+  $(".poi-container").on("click", ".poi-title", function(event) {
 
-    console.log('bang');
+
 
     // $(this).find(".poi-body").slideToggle("slow");
 
     // $(this).next().find(".poi-body").slideToggle("slow");
 
 
-    $(this).next().slideToggle("slow");
 
+
+
+    // if(event.target.id != target) {
+
+    $(".poi-body").slideUp("slow");
+
+    console.log('bang');
+
+    // }
+
+    $(this).next().slideDown("slow");
 
   });
 

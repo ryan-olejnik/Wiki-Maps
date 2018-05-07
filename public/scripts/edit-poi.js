10592e4a0ab9885a709f5801153728e08321c70a
Justin <j.lum07@gmail.com>
2018-05-06 21:14:35 -0400

    fixing up edit

--- public/scripts/edit-poi.js
+++ public/scripts/edit-poi.js
@@ -1,16 +1,13 @@
 $( document ).ready(function() {
 
-
-  console.log(poi_data);
-
   document.getElementById('poi-title').value = poi_data[0].title;
   document.getElementById('poi-address').value = poi_data[0].address;
   document.getElementById('poi-description').value = poi_data[0].description;
-  document.getElementById('gps-lat').value = poi_data[0].latitude;
-  document.getElementById('gps-lng').value = poi_data[0].longitude;
-  document.getElementById('placeid').value = poi_data[0].place_id;
+  // document.getElementById('gps-lat').value = poi_data[0].latitude;
+  // document.getElementById('gps-lng').value = poi_data[0].longitude;
+  // document.getElementById('placeid').value = poi_data[0].place_id;
   document.getElementById('image').value = poi_data[0].image;
-
+  document.getElementById('map-id').value = poi_data[0].map_id;
 });
 
 

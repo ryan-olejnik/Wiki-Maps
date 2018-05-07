9bb39740cc98e54390fe54a3f8e52e933eb8965c
Justin <j.lum07@gmail.com>
2018-05-07 00:48:43 -0400

    🚀 added like icon color changing

--- public/scripts/view-map-list.js
+++ public/scripts/view-map-list.js
@@ -1,28 +1,29 @@
 $( document ).ready(function() {
 
+
+  // Opening and closing body contents
   $(".poi-container").on("click", ".poi-title", function(event) {
+    // If to see if div is alreay body
+    if (!$(this).next().is(":visible")) {
+      $(".poi-body").slideUp("slow");
+      $(this).next().slideDown("slow");
+    } else {
+      $(this).next().slideUp("slow");
+    }
+
+  });
 
 
+  $('#like-button').on('click', function(event) {
 
-    // $(this).find(".poi-body").slideToggle("slow");
+    event.preventDefault();
 
-    // $(this).next().find(".poi-body").slideToggle("slow");
-
-
-
-
-
-    // if(event.target.id != target) {
-
-    $(".poi-body").slideUp("slow");
-
-    console.log('bang');
-
-    // }
-
-    $(this).next().slideDown("slow");
+    if ($(this).find('#like-icon').hasClass('unliked')) {
+      $(this).find('#like-icon').removeClass('far fa-heart fa-2x unliked').addClass('fas fa-heart fa-2x liked');
+    } else {
+      $(this).find('#like-icon').removeClass('fas fa-heart fa-2x liked').addClass('far fa-heart fa-2x unliked');
+    }
 
   });
 
 });
-
